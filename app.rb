51148af12f1b2277b869993b114b10caa0b2590d
Tsvi Tannin <dudebroman17@gmail.com>
2013-07-23 17:37:53 -0400

    file structure hopefully

--- app.rb
+++ app.rb
@@ -1,32 +1,32 @@
 class App < Sinatra::Base
-require 'sinatra'
+	require 'sinatra'
 
-get '/' do
-  erb :index
-end
+	get '/' do
+	  erb :index
+	end
 
-get '/about' do
-	"This is the about page"
-end
+	get '/about' do
+		"This is the about page"
+	end
 
-get '/hello/:name' do 
-	name=params[:name];
-	"Hey there, #{name}."
-end
-get '/form' do
-	erb :form
-end
+	get '/hello/:name' do 
+		name=params[:name];
+		"Hey there, #{name}."
+	end
+	get '/form' do
+		erb :form
+	end
 
-get '/send' do
-	erb :send
-end
-post '/form' do
-	"I see you added your last name Mr. #{params[:message]}"
-end
+	get '/send' do
+		erb :send
+	end
+	post '/form' do
+		"I see you added your last name Mr. #{params[:message]}"
+	end
 
-["/foo", "/bar", "/baz"].each do |path|
-  get path do
-    "You've reached me at #{request.path_info}"
-  end
-end
+	["/foo", "/bar", "/baz"].each do |path|
+	  get path do
+	    "You've reached me at #{request.path_info}"
+	  end
+	end
 end
